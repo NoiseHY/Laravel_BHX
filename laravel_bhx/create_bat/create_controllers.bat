7cc7da4a58192e34673921d31adbd13d2004958d
NoiseHY <congnamhy1@gmail.com>
2024-03-31 15:15:23 +0700

    First Step
    Tạo các model, controllers

---
+++ laravel_bhx/create_bat/create_controllers.bat
@@ -0,0 +1,7 @@
+
+php artisan make:controller LoaiSanPhamControllers --resource
+php artisan make:controller SanPhamControllers --resource
+php artisan make:controller KhachHangControllers --resource
+php artisan make:controller NguoiDungControllers --resource
+php artisan make:controller HoaDonControllers --resource
+php artisan make:controller ChiTietHoaDonControllers --resource
---
+++ laravel_bhx/create_bat/create_model.bat
@@ -0,0 +1,7 @@
+
+php artisan make:model LoaiSanPham
+php artisan make:model SanPham
+php artisan make:model KhachHang 
+php artisan make:model NguoiDung 
+php artisan make:model HoaDon 
+php artisan make:model ChiTietHoaDon 
